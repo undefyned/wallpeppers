16733110d7f709ea08f665aeea0af67b821354ce
undefyned <gamervxs@gmail.com>
2020-07-05 11:57:06 +0530

    Add files via upload

---
+++ allpeppers.js
@@ -0,0 +1,414 @@
+wallsData = [
+{
+        "name": "Aquarius",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/AQUARIUS-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/AQUARIUS-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Aries",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/ARIES-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/ARIES-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Cancer",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/CANCER-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/CANCER-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Capricorn",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/CAPRICORN-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/CAPRICORN-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Gemini",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/GEMINI-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/GEMINI-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Leo",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/LEO-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/LEO-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Libra",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/LIBRA-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/LIBRA-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Pisces",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/PISCES-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/PISCES-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Sagittarius",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/SAGITTARIUS-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/SAGITTARIUS-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Scorpio",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/SCORPIO-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/SCORPIO-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Taurus",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/TAURUS-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/TAURUS-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Virgo",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations-amoled/VIRGO-amoled.png",
+        "thumbnail": "wallpeppers/thumbs/constellations-amoled/VIRGO-amoled.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Aquarius",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/AQUARIUS.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/AQUARIUS.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Aries",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/ARIES.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/ARIES.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Cancer",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/CANCER.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/CANCER.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Capricorn",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/CAPRICORN.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/CAPRICORN.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Gemini",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/GEMINI.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/GEMINI.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Leo",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/LEO.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/LEO.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Libra",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/LIBRA.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/LIBRA.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Pisces",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/PISCES.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/PISCES.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Sagittarius",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/SAGITTARIUS.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/SAGITTARIUS.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Scorpio",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/SCORPIO.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/SCORPIO.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Taurus",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/TAURUS.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/TAURUS.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Virgo",
+        "author": "@imundefyned",
+        "url": "wallpeppers/constellations/VIRGO.png",
+        "thumbnail": "wallpeppers/thumbs/constellations/VIRGO.webp",
+        "collections": "#CONSTELLATION"
+}, {
+        "name": "Wow A Shark",
+        "author": "@imundefyned",
+        "url": "wallpeppers/minimal/WOW%20A%20SHARK.png",
+        "thumbnail": "wallpeppers/thumbs/minimal/WOW%20A%20SHARK.webp",
+        "collections": "#MINIMAL"
+}, {
+        "name": "Knowerehere",
+        "author": "@imundefyned",
+        "url": "wallpeppers/minimal/KNOWEREHERE.png",
+        "thumbnail": "wallpeppers/thumbs/minimal/KNOWEREHERE.webp",
+        "collections": "#MINIMAL"
+}, {
+        "name": "Knowerehere Dark",
+        "author": "@imundefyned",
+        "url": "wallpeppers/minimal/KNOWEREHERE%20DARK.png",
+        "thumbnail": "wallpeppers/thumbs/minimal/KNOWEREHERE%20DARK.webp",
+        "collections": "#MINIMAL"
+}, {
+        "name": "Desert",
+        "author": "@imundefyned",
+        "url": "wallpeppers/minimal/DESERT.png",
+        "thumbnail": "wallpeppers/thumbs/minimal/DESERT.webp",
+        "collections": "#MINIMAL"
+}, {
+        "name": "Desert Night",
+        "author": "@imundefyned",
+        "url": "wallpeppers/minimal/DESERT%20NIGHT.png",
+        "thumbnail": "wallpeppers/thumbs/minimal/DESERT%20NIGHT.webp",
+        "collections": "#MINIMAL"
+}, {
+        "name": "Balloons",
+        "author": "@imundefyned",
+        "url": "wallpeppers/space/BALLOONS.png",
+        "thumbnail": "wallpeppers/thumbs/space/BALLOONS.webp",
+        "collections": "#SPACE"
+}, {
+        "name": "Balloons",
+        "author": "@imundefyned",
+        "url": "wallpeppers/space/BALLOONS%20AMOLED.png",
+        "thumbnail": "wallpeppers/thumbs/space/BALLOONS%20AMOLED.webp",
+        "collections": "#SPACE"
+}, {
+        "name": "Gradient P Dark",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/GRADIENT%20P%20DARK.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/GRADIENT%20P%20DARK.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Gradient P Light",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/GRADIENT%20P%20LIGHT.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/GRADIENT%20P%20LIGHT.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Gradient BT",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/GRADIENT%20BT.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/GRADIENT%20BT.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Gradient Midnight",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/GRADIENT%20MIDNIGHT.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/GRADIENT%20MIDNIGHT.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Gradient X",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/GRADIENT%20X.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/GRADIENT%20X.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Iridescent One",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/IRIDESCENT%20ONE.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/IRIDESCENT%20ONE.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Iridescent Two",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/IRIDESCENT%20TWO.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/IRIDESCENT%20TWO.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Rainbow",
+        "author": "@imundefyned",
+        "url": "wallpeppers/gradients/RAINBOW.png",
+        "thumbnail": "wallpeppers/thumbs/gradients/RAINBOW.webp",
+        "collections": "#GRADIENTS"
+}, {
+        "name": "Tri Neon",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/TRI%20NEON.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/TRI%20NEON.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Tri Spiral",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/TRI%20SPIRAL.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/TRI%20SPIRAL.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Eleven",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/ELEVEN.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/ELEVEN.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Eleven AMOLED",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/ELEVEN%20AMOLED.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/ELEVEN%20AMOLED.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Buttons",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/BUTTONS.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/BUTTONS.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Buttons AMOLED",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/BUTTONS%20AMOLED.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/BUTTONS%20AMOLED.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Bubbles Amber",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/BUBBLES%20AMBER.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/BUBBLES%20AMBER.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Bubbles Teal",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/BUBBLES%20TEAL.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/BUBBLES%20TEAL.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Graph",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/GRAPH%20AMOLED.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/GRAPH%20AMOLED.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Graph",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/GRAPH.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/GRAPH.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Gradient RP",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/GRADIENT%20RP.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/GRADIENT%20RP.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "A Wallpepper",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/A%20WALLPEPPER.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/A%20WALLPEPPER.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Split Triangle",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/SPLIT%20TRIANGLE.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/SPLIT%20TRIANGLE.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Plus Pattern",
+        "author": "@imundefyned",
+        "url": "wallpeppers/patterns/PLUS%20PATTERN.png",
+        "thumbnail": "wallpeppers/thumbs/patterns/PLUS%20PATTERN.webp",
+        "collections": "#PATTERNS"
+}, {
+        "name": "Retrowave Two",
+        "author": "@imundefyned",
+        "url": "wallpeppers/inspired/RETROWAVE%20TWO.png",
+        "thumbnail": "wallpeppers/thumbs/inspired/RETROWAVE%20TWO.webp",
+        "collections": "#INSPIRED"
+}, {
+        "name": "Retrowave One",
+        "author": "@imundefyned",
+        "url": "wallpeppers/inspired/RETROWAVE%20ONE.png",
+        "thumbnail": "wallpeppers/thumbs/inspired/RETROWAVE%20ONE.webp",
+        "collections": "#INSPIRED"
+}, {
+        "name": "iOS Inspired",
+        "author": "@imundefyned",
+        "url": "wallpeppers/inspired/IOS%20INSPIRED.png",
+        "thumbnail": "wallpeppers/thumbs/inspired/IOS%20INSPIRED.webp",
+        "collections": "#INSPIRED"
+}, {
+        "name": "macOS Inspired",
+        "author": "@imundefyned",
+        "url": "wallpeppers/inspired/MACOS%20INSPIRED.png",
+        "thumbnail": "wallpeppers/thumbs/inspired/MACOS%20INSPIRED.webp",
+        "collections": "#INSPIRED"
+}, {
+        "name": "NURKO",
+        "author": "@imundefyned",
+        "url": "wallpeppers/exclusive/NURKO%20X%20RETROWAVE.png",
+        "thumbnail": "wallpeppers/thumbs/exclusive/NURKO%20X%20RETROWAVE.webp",
+        "title": "Nurko x Retrowave",
+        "artist": "&nbsp;",
+        "spotify": "https://open.spotify.com/artist/757FXqX0Osk2pqtgv4E5v4",
+        "apple": "https://music.apple.com/us/artist/nurko/1060269286"
+}, {
+        "name": "Apple Pie",
+        "author": "@imundefyned",
+        "url": "wallpeppers/exclusive/APPLE%20PIE.png",
+        "thumbnail": "wallpeppers/thumbs/exclusive/APPLE%20PIE.webp",
+        "title": "Apple Pie",
+        "artist": "by XYLØ",
+        "spotify": "https://open.spotify.com/track/0B8deSo9mvlerSmsAYOWnB",
+        "apple": "https://music.apple.com/us/album/apple-pie/1512548029"
+}, {
+        "name": "Love Is Gone",
+        "author": "@imundefyned",
+        "url": "wallpeppers/exclusive/LOVE%20IS%20GONE.png",
+        "thumbnail": "wallpeppers/thumbs/exclusive/LOVE%20IS%20GONE.webp",
+        "title": "Love Is Gone",
+        "artist": "by SLANDER (ft. Dylan Matthew)",
+        "spotify": "https://open.spotify.com/track/39glqzRVRAy4vq3PqeTGb8",
+        "apple": "https://music.apple.com/us/album/love-is-gone-feat-dylan-matthew/1462340581"
+}, {
+        "name": "Love Is Gone",
+        "author": "@imundefyned",
+        "url": "wallpeppers/exclusive/LOVE%20IS%20GONE%20AMOLED.png",
+        "thumbnail": "wallpeppers/thumbs/exclusive/LOVE%20IS%20GONE%20AMOLED.webp",
+        "title": "Love Is Gone",
+        "artist": "by SLANDER (ft. Dylan Matthew)",
+        "spotify": "https://open.spotify.com/track/39glqzRVRAy4vq3PqeTGb8",
+        "apple": "https://music.apple.com/us/album/love-is-gone-feat-dylan-matthew/1462340581"
+}, {
+        "name": "Love Is Gone 2",
+        "author": "@imundefyned",
+        "url": "wallpeppers/exclusive/LOVE%20IS%20GONE%202.png",
+        "thumbnail": "wallpeppers/thumbs/exclusive/LOVE%20IS%20GONE%202.webp",
+        "title": "Love Is Gone",
+        "artist": "by SLANDER (ft. Dylan Matthew)",
+        "spotify": "https://open.spotify.com/track/39glqzRVRAy4vq3PqeTGb8",
+        "apple": "https://music.apple.com/us/album/love-is-gone-feat-dylan-matthew/1462340581"
+}, {
+        "name": "Love Is Gone 2",
+        "author": "@imundefyned",
+        "url": "wallpeppers/exclusive/LOVE%20IS%20GONE%202%20AMOLED.png",
+        "thumbnail": "wallpeppers/thumbs/exclusive/LOVE%20IS%20GONE%202%20AMOLED.webp",
+        "title": "Love Is Gone",
+        "artist": "by SLANDER (ft. Dylan Matthew)",
+        "spotify": "https://open.spotify.com/track/39glqzRVRAy4vq3PqeTGb8",
+        "apple": "https://music.apple.com/us/album/love-is-gone-feat-dylan-matthew/1462340581"
+}
+];
+
+function peppers(wall) {
+    return `
+        <div class="flex-container">
+            <div><img src="${wall.url}" class="img-item"></div>
+        </div>
+`
+}
+
+document.getElementById("app").innerHTML = `
+    <span class="subheading">Total Wallpaper Count: ${wallsData.length}</span><br>
+    ${  
+        wallsData.map(peppers).join("")
+    }
+    `;
